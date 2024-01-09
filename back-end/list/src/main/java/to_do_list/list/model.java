46bb2e407c64aa1bf5acc290489c56c3a78c9b35
klark <daniil-larkovich@mail.ru>
2024-01-10 00:43:40 +0600

    add Entity

---
+++ back-end/list/src/main/java/to_do_list/list/model.java
@@ -0,0 +1,66 @@
+// import javax.persistence.Entity;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.GenerationType;
+// import javax.persistence.Id;
+// package to_do_list.list;
+
+// public class model {
+
+// @Entity
+// public class Employee {
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
+
+//     private String name;
+//     private Integer age;
+//     private BigDecimal salary;
+
+//     // Конструкторы
+
+//     public Employee() {}
+
+//     public Employee(String name, Integer age, BigDecimal salary) {
+//         this.name = name;
+//         this.age = age;
+//         this.salary = salary;
+//     }
+
+//     // Геттеры и сеттеры
+
+//     public Long getId() {
+//         return id;
+//     }
+
+//     public void setId(Long id) {
+//         this.id = id;
+//     }
+
+//     public String getName() {
+//         return name;
+//     }
+
+//     public void setName(String name) {
+//         this.name = name;
+//     }
+
+//     public Integer getAge() {
+//         return age;
+//     }
+
+//     public void setAge(Integer age) {
+//         this.age = age;
+//     }
+
+//     public BigDecimal getSalary() {
+//         return salary;
+//     }
+
+//     public void setSalary(BigDecimal salary) {
+//         this.salary = salary;
+//     }
+
+// }
+
+    
+// }
